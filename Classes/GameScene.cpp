f6bbdc878d52a51b4ea4fef154b260c5a50cc4d1
Pisces000221 <1786762946@qq.com>
2014-06-07 23:49:41 +0800

    A better tile, and a test

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -11,22 +11,26 @@ bool GameLayer::init()
     Size size = Director::getInstance()->getVisibleSize();
     Vec2 centre = Vec2(size.width * 0.5, HEX_SIDE_LEN * 2.5);
 
-    for (int i = -size.width * 0.5 / HEX_DIAMETRE - 2; i <= size.width * 0.5 / HEX_DIAMETRE + 2; i++)
-        for (int j = -centre.y / HEX_HEIGHT - 1; j <= (size.height - centre.y) / HEX_HEIGHT + 1; j++) {
+    // a bigger i means a bigger y
+    for (int i = -centre.y / HEX_HEIGHT - 1; i <= (size.height - centre.y) / HEX_HEIGHT + 2; i++)
+        for (int j = -size.width * 0.5 / HEX_DIAMETRE - 2; j <= size.width * 0.5 / HEX_DIAMETRE + 2; j++) {
             Vec2 p;
-            if (i % 2) p = Vec2(i * HEX_SIDE_LEN * 1.5, j * HEX_HEIGHT) + centre;
-            else p = Vec2(i * HEX_SIDE_LEN * 1.5, (j + 0.5) * HEX_HEIGHT) + centre;
+            if (i % 2) p = Vec2(j * HEX_HEIGHT, i * HEX_SIDE_LEN * 1.5) + centre;
+            else p = Vec2((j + 0.5) * HEX_HEIGHT, i * HEX_SIDE_LEN * 1.5) + centre;
             // create a solid hexagon (I'm too lazy to use a DrawNode)
             auto hf = Sprite::create("images/hexagon_fill.png");
             hf->setPosition(p);
-            hf->setColor(Color3B(0, 0, 0));
-            this->addChild(hf, 0, HEXAGON_TAG(i, j) - 9);   // border tag - 9 = filling tag
+            hf->setColor(Color3B(0, 255, 0));
+            this->addChild(hf, 0, HEXAGON_TAG(j, i) - 9);   // border tag - 9 = filling tag
             // create its border
             auto hb = Sprite::create("images/hexagon_border.png");
             hb->setPosition(p);
-            this->addChild(hb, 1, HEXAGON_TAG(i, j));
+            this->addChild(hb, 1, HEXAGON_TAG(j, i));
         }
     this->getChildByTag(HEXAGON_TAG(0, 0) - 9)->setColor(Color3B(255, 255, 0));
+    //this->runAction(ScaleTo::create(3, 0.5));
+    this->getChildByTag(HEXAGON_TAG(1, 1))->setColor(Color3B(255, 255, 0));
+    this->runAction(MoveTo::create(1, centre - this->getChildByTag(HEXAGON_TAG(1, 1))->getPosition()));
 
     return true;
 }
